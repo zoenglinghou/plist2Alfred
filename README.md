a98e44757909b3d63b162a4ffb117bbee99bff89
CarlosEvo <zhanglh.carlos2012@gmail.com>
2017-11-17 13:28:23 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Introduction
+
+`plist2Alfred.py` converts a macOS text substitution `.plist` to a `.alfredsnippets`.
+
+# Prior work
+
+The `LaTeX Subsitutions.plist` comes from [LaTeXSubstitutionPlist](https://github.com/deszoeke/LaTeXSubstitutionsPlist).
+
+The original Python script comes from [alfred-snippets](https://github.com/AndiH/alfred-snippets/blob/master/convert-aText-to-Alfred.py), and I modified it to fit my need.
---
+++ plist2Alfred.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import json
+import os
+import plistlib
+import tempfile
+import uuid
+import zipfile
+
+import click
+
+def read_plist(filename):
+    with open(filename, 'rb') as plist_file:
+        plist_entry = plistlib.load(plist_file)
+    return plist_entry
+
+
+def gen_uuid():
+    return str(uuid.uuid4())
+
+
+def plist_to_alfred(plist_entry):
+    alfred_entry = []
+    for entry in plist_entry:
+        temp_dict = dict()
+        temp_dict["snippet"] = entry["phrase"]
+        temp_dict["uid"] = gen_uuid()
+        temp_dict["name"] = entry["shortcut"][1:]
+        temp_dict["keyword"] = entry["shortcut"] + '\\'
+        snippet = {"alfredsnippet": temp_dict}
+        alfred_entry.append(snippet)
+    return alfred_entry
+
+
+def save_json(entry):
+    filename = entry["alfredsnippet"]["name"] + " " + entry["alfredsnippet"]["uid"] + ".json"
+    with open(filename, "w") as outfile:
+        json.dump(entry, outfile)
+    return outfile.name
+
+
+@click.command()
+@click.argument("filename", type=click.Path(exists=True))
+def main(filename):
+    """Convert plist text substitution to Alfred Snippets (2017 by CarlosEvo)
+
+    Small tool which reads maxOS text substitution plist files and generates an Alfred (3,x) snippet collection.
+
+    The script will work in a temporary directory and move the resulting .alfredsnippets file back to the directory it is invoked from.
+
+    """
+
+    plist_entry = read_plist(filename)
+    alfred_entry = plist_to_alfred(plist_entry)
+    current_dir = os.getcwd()
+    with tempfile.TemporaryDirectory() as temp_dir:
+        os.chdir(temp_dir)
+        file_list = []
+        for entry in alfred_entry:
+            file_list.append(save_json(entry))
+        output_filename = filename[:-4] + '.alfredsnippets'
+        with zipfile.ZipFile(output_filename, 'w', zipfile.ZIP_DEFLATED) as zf:
+            for entry in file_list:
+                zf.write(entry)
+            zf.close()
+        os.rename(output_filename, os.path.join(current_dir, output_filename))
+
+
+if __name__ == '__main__':
+    main()
